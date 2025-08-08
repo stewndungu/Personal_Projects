85decdf6ed174c3fb30ab39f47debd3474f2fca1
stewndungu <110838587+stewndungu@users.noreply.github.com>
2025-08-08 09:36:58 -0500

    Update trial.bat

--- trial.bat
+++ trial.bat
@@ -1,5 +1,6 @@
 
 set filename=report_%date:~-10,2%_%date:~-7,2%_%date:~-4,4%_%time:~0,2%%time:~3,2%.txt
 cd C:\xampp\mysql\bin
-mysql -u root -h 127.0.0.1 < "C:\Users\Owner\Desktop\query.sql"  >> "C:\Users\Owner\Desktop\Inventory_reports\%filename%"
+mysql -u root -h 127.0.0.1 < "C:\path-to-querys.sql\query.sql"  >> "C:\Path-to-your-folder-with-items\%filename%"
+
 
