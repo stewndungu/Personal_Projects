deb7d17365337e549ab2e734ab9f3c6245685797
stewndungu <110838587+stewndungu@users.noreply.github.com>
2025-09-28 08:17:25 -0500

    Update query.bat

--- query.bat
+++ query.bat
@@ -1,16 +1,17 @@
 use inventory;
-SELECT id,brand,label, Calendar, 'tobacco' AS source
+SELECT id,brand,label, Calendar, '** some table **' AS source
 FROM tobacco
 WHERE Calendar BETWEEN CURDATE() AND DATE_ADD(CURDATE(), INTERVAL 1 MONTH)
 
 UNION ALL
 
-SELECT id,brand,label, Calendar, 'medicine' AS source
+SELECT id,brand,label, Calendar, ''** some table **' AS source
 FROM medicine
 WHERE Calendar BETWEEN CURDATE() AND DATE_ADD(CURDATE(), INTERVAL 1 MONTH)
 
 UNION ALL
 
-SELECT id,brand,label, Calendar, 'beverages' AS source
+SELECT id,brand,label, Calendar, ''** some table **' AS source
 FROM beverages
+
 WHERE Calendar BETWEEN CURDATE() AND DATE_ADD(CURDATE(), INTERVAL 1 MONTH);
